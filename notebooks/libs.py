19d8c2273bc2a03d21daa0419c7fbe01e5d0dc13
Camille Scott <camille.scott.w@gmail.com>
2015-05-13 21:03:59 -0400

    Update the common notebook resources

---
+++ notebooks/libs.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env ipython
+from __future__ import print_function
+
+from itertools import izip
+import os
+import sys
+import json
+from collections import defaultdict as ddict
+from pprint import pprint
+import warnings
+
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn2, venn3, venn3_circles
+
+import seaborn as sns
+
+import screed
+
+from peasoup.plot import FigManager
+from peasoup.plot import plot_dendro
+
+import pandas as pd
+from pandas import Categorical
+import numpy as np
+from numpy.random import rand
+from scipy import stats 
+from scipy.spatial.distance import pdist
+from scipy.spatial.distance import squareform
+import scipy.cluster.hierarchy as sch
+
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.cluster import KMeans
+
+from IPython.display import SVG
+from IPython.display import FileLink
+from IPython.display import HTML
+
+import mygene
+from bioservices import UniProt
