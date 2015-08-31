1132e443c7450194646e7cc13c338ab14015d8e3
Camille Scott <camille.scott.w@gmail.com>
2015-08-31 12:05:06 -0700

    Finish off annotate task

--- annotate.py
+++ annotate.py
@@ -17,15 +17,18 @@ import screed
 
 from tasks import *
 import blasttools
+from blasttools import outfmt6
 import hmmertools
 
-def annotate_task(assembly_fn, blast_targets, transdecoder_fn, hmmscan_fn,
-                  busco_fn, annotation_fn):
+@create_task_object
+def aggregate_annotations_task(assembly_fn, blast_targets, transdecoder_fn, hmmscan_fn,
+                               sample_df, annotation_fn):
 
     def get_store():
         return pd.HDFStore(annotation_fn, complib='zlib', complevel=5)
 
-    def annotate():
+    def get_assembly_df():
+        print('--- Building initial assembly DataFrame...')
         store = get_store()
         idx = []
         lens = []
@@ -34,11 +37,19 @@ def annotate_task(assembly_fn, blast_targets, transdecoder_fn, hmmscan_fn,
             lens.append(len(record.sequence))
         annot_df = pd.DataFrame({'length': lens}, index=idx)
         print(annot_df.head())
+        store['annot_df'] = annot_df
+        store.close()
 
+    best_hits_files = []
+    for db_fn in blast_targets:
+        target = '{}.x.{}.db.tsv'.format(assembly_fn, db_fn)
+        name = 'x_{}'.format(strip_seq_extension(db_fn))
+        best_hits_files.append((name, target))
+
+    def get_best_hits():
         print('--- Calculating best hits...')
-        for i, db_fn in enumerate(blast_targets):
-            target = '{}.x.{}.db.tsv'.format(assembly_fn, db_fn)
-            name = 'x_{}'.format(strip_seq_extension(db_fn))
+        store = get_store()
+        for i, (name, target) in enumerate(best_hits_files):
             print('\t...working on {}'.format(target))
             
             df = blasttools.blast_to_df(target)
@@ -54,19 +65,30 @@ def annotate_task(assembly_fn, blast_targets, transdecoder_fn, hmmscan_fn,
                 best_hits_panel = pd.Panel({name: tmp})
             else:
                 best_hits_panel[name] = tmp
+        store['best_hits_panel'] = best_hits_panel
+        best_hits = best_hits_panel.minor_xs('sseqid')
+        store['best_hits_df'] = best_hits
+        #store['blast_filter_df'] = best_hits.minor_xs('evalue') >= 0
+        store.close()
 
+    ortho_files = []
+    for db_fn in blast_targets:
+        A_fn = '{}.x.{}.db.tsv'.format(assembly_fn, db_fn)
+        B_fn = '{}.db.x.{}.tsv'.format(db_fn, assembly_fn)
+        name = 'x_'.format(strip_seq_extension(db_fn))
+        ortho_files.append((name, A_fn, B_fn))
+
+    def get_orthologies():
         print('--- Calculating recipricol best hits...')
-        for i, db_fn in enumerate(blast_targets):
-            A_fn = '{}.x.{}.db.tsv'.format(assembly_fn, db_fn)
-            B_fn = '{}.db.x.{}.tsv'.format(db_fn, assembly_fn)
-            name = 'x_'.format(strip_seq_extension(db_fn))
+        store = get_store()
+        for i, (name, A_fn, B_fn) in enumerate(ortho_files):
             print('\t...working on {}, {}'.format(A_fn, B_fn))
             
-            A = pd.read_table(A_fn, header=None, index_col=0, names=outfmt6)
-            B = pd.read_table(B_fn, header=None, index_col=0, names=outfmt6)
+            A = blasttools.blast_to_df(A_fn)
+            B = blasttools.blast_to_df(B_fn)
 
-            fix_blast_coords_df(A)
-            fix_blast_coords_df(B)
+            remap_blast.remap_blast_coords_df(A)
+            remap_blast.remap_blast_coords_df(B)
             
             X = blasttools.get_orthologies(A, B, tpm_df.index)
             
@@ -74,12 +96,50 @@ def annotate_task(assembly_fn, blast_targets, transdecoder_fn, hmmscan_fn,
                 ortho_panel = pd.Panel({name: X})
             else:
                 ortho_panel[name] = X
+        store['ortho_panel'] = ortho_panel
+        orthos = ortho_panel.minor_xs('sseqid')
+        store['ortho_df'] = orthos
+        #store['ortho_filter_df'] = orthos.minor_xs('evalue') >= 0
+        store.close()
 
+    def get_transdecoder():
         print('--- Parsing TransDecoder results...')
+        store = get_store()
         transdecoder_df = hmmertools.gff3_transdecoder_to_df(transdecoder_fn)
+        store['transdecoder_dr'] = transdecoder_df
+        store.close()
+    
 
+    def get_pfam():
         print('--- Parsing HMMER P-fam results...')
+        store = get_store()
         pfam_df = hmmertools.hmmscan_to_df(hmmscan_fn)
+        store['pfam_df'] = pfam_df
+        store.close()
 
-        print('--- Aggregating...')
+    def get_tpm():
+        print('--- Getting expression data...')
+        store = get_store()
+        tpm_df = pd.read_csv(tpm_fn, delimiter='\t', index_col=0)
+        labels = dict(zip(sample_df.filename, sample_df.label))
+        tpm_df.rename(columns=labels, inplace=True)
+        tpm_df.sort(axis=1, inplace=True)
+        store['tpm_df'] = tpm_df
+        store.close()
 
+    actions = [(get_assembly_df, []), (get_best_hits, []), (get_orthologies, []),
+               (get_transdecoder, []), (get_pfam, []), (get_tpm, [])]
+
+    blast_file_dep = []
+    for _, fn in best_hits_files:
+        blast_file_dep.append(fn)
+    for _, A_fn, B_fn in ortho_files:
+        blast_file_dep.extend([A_fn, B_fn])
+    file_dep = [assembly_fn, transdecoder_fn, hmmscan_fn] + blast_file_dep
+
+    return {'title': title_with_actions,
+            'name': 'aggregate_annotations_' + assembly_fn,
+            'actions': actions,
+            'file_dep': file_dep,
+            'targets': [annotation_fn],
+            'clean': [clean_targets]}
