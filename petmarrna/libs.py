7e0d3befbd14d59e2d4ffb61972407bff28dc174
Camille Scott <camille.scott.w@gmail.com>
2015-09-14 21:32:20 -0700

    Restructure to proper package format

--- petmarrna/libs.py
+++ petmarrna/libs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env ipython
 from __future__ import print_function
 
+# Standard library packages
+
 from itertools import izip
 import os
 import sys
@@ -9,41 +11,43 @@ from collections import defaultdict as ddict
 from pprint import pprint
 import warnings
 
-import matplotlib as mpl
-import matplotlib.pyplot as plt
-from matplotlib_venn import venn2, venn3, venn3_circles
+# Dependencies
 
-import seaborn as sns
-
-from figuremanager import FigManager
-
-import screed
-
-import pandas as pd
-from pandas import Categorical
-import numpy as np
-from numpy.random import rand
-from scipy import stats
-from scipy.spatial.distance import pdist
-from scipy.spatial.distance import squareform
-import scipy.cluster.hierarchy as sch
-
-from sklearn.decomposition import PCA, KernelPCA
-from sklearn.cluster import KMeans
+from bioservices import UniProt
 
 from IPython.display import SVG
 from IPython.display import FileLink
 from IPython.display import HTML
 
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+from matplotlib_venn import venn2, venn3, venn3_circles
+
 import mygene
-from bioservices import UniProt
 
+import numpy as np
+from numpy.random import rand
 
-cwd = os.getcwd()
-sys.path.insert(0, os.path.join('../', cwd))
+import pandas as pd
+from pandas import Categorical
+
+import screed
+
+from scipy import stats
+from scipy.spatial.distance import pdist
+from scipy.spatial.distance import squareform
+import scipy.cluster.hierarchy as sch
+
+import seaborn as sns
+
+from sklearn.decomposition import PCA, KernelPCA
+from sklearn.cluster import KMeans
+
+# Local modules
 
 import blasttools
 import buscotools
 import hmmertools
+from figuremanager import FigManager
 
 
